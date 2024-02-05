c2308093dd77dadd29a5188f522f09b9bb8e3139
DazzyDazz-x <111580056+DazzyDazz-x@users.noreply.github.com>
2024-02-05 18:07:18 +0100

    Ejemplos While

---
+++ While.py
@@ -0,0 +1,26 @@
+""" #Imprimir de 1 al 9
+i=1
+while i < 10:
+    print(i)
+    i=+ 1
+ """
+
+#Imprimir los valores desde 50 hasta 100.
+""" i=50
+while i <= 100:
+    print(i)
+    i+= 1 """
+
+#Desde 5, imprimir los valores 5 a 20, pero excluye 12.
+""" i= 5
+while i<=20:
+    if i>=5 and i<12: 
+        print(i)
+    elif i>12:   
+        print(i)
+    i+= 1 
+ """
+
+#Preguntar al usuario por números hasta que el usuario introduzca “q” para quit.  Sumar los valores y imprimir el resultado final. 
+
+Numeros= int(input("Introduce los numeros que quieras. Para terminar presiona 'q' :  "))
