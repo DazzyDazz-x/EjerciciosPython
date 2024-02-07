c367eb13ab32be20ea18cf80cddd828508e6fd0a
DazzyDazz-x <111580056+DazzyDazz-x@users.noreply.github.com>
2024-02-07 12:57:09 +0100

    Ejercicio de password

---
+++ Password.py
@@ -0,0 +1,8 @@
+Passwd=input("Introduce tu contrasena: ")
+Passwd2= input("Introduce de nuevo tu contrasena: ")
+
+while Passwd != Passwd2:
+    print("Las contrasenas no coinciden. Hacerlo de nuevo")
+    Passwd=input("Introduce tu contrasena: ")
+    Passwd2= input("Introduce de nuevo tu contrasena: ")
+print("Muy bien. Las contrasenas coinciden")    
