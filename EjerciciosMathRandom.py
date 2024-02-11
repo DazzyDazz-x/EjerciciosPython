e365dcabe423706541bde5b02ca4dd2db2df02c2
DazzyDazz-x <111580056+DazzyDazz-x@users.noreply.github.com>
2024-02-11 22:27:12 +0100

    Ejercicios con las librerias de  Math  y Random

---
+++ EjerciciosMathRandom.py
@@ -0,0 +1,26 @@
+from math import *
+import random
+from statistics import *
+
+#print(sqrt(16))
+
+#v1=random.random()
+#v2= random.randint(0,100)
+#print(v1)
+
+#Sacar de la lista, un alumno de forma aleatoria
+""" nombres=["Arturo", "Julio", "Dani","Aurora", "Roberto", "Martina"]
+print(random.choice(nombres))
+
+#Media
+notas=[2,6,7,3,7,8,6]
+print("La media es:", mean(notas) ) """
+
+x= 15.6547
+print(ceil(x))
+print(floor(x))
+print(pow(4,3))
+print(gcd(2,8))
+print(factorial(4))
+print(e)
+print(pi)
