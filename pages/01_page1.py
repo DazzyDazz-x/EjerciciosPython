164c3f3bae242dde0c4ff202f0a666babc3b9f89
Daisy <dras2002@hotmail.com>
2024-02-27 17:52:42 +0100

    Clase_Ejercicios27_02_2024

--- pages/01_page1.py
+++ pages/01_page1.py
@@ -1,3 +1,14 @@
 import streamlit as st
+from datetime import datetime  #porque asi te aparecen mas opciones para usar
 
-st.title("Mi pagina 1")
+
+
+with st.form("my form", clear_on_submit=True):
+    nom= st.text_input("## *Nombre*: ")
+    pro= st.text_area("## *Describe tu problema tecnico:* ")
+
+    if st.form_submit_button("Generar y Guardar Informe"):
+        st.write( pro)
+        with open("InformeProbTecnico.txt", "a") as f:
+            descripcion= nom + "-" + pro + "-" + str( datetime.now()) #.strftimr("%yy%M%d")
+            f.write( descripcion +"\n")
--- pages/02_page2.py
+++ pages/02_page2.py
@@ -1,3 +1,38 @@
 import streamlit as st
 
-st.title("Mi pagina 2")
+import streamlit as st
+from datetime import datetime  #porque asi te aparecen mas opciones para usar
+
+
+st.title("Servicio Tecnico 2")
+
+#with st.form("my form", clear_on_submit=True):
+nom= st.text_input("## *Nombre*: ")
+pro= st.text_area("## *Describe tu problema tecnico:* ")
+    
+if st.button("Guardar Informe"):
+            
+    with open("InformeProbTecnico.txt", "a") as f:
+        descripcion= nom + "-" + pro + "-" + str( datetime.now()) #.strftimr("%yy%M%d")
+        f.write( descripcion +"\n")
+
+if st.button("Mostrar Informe"):
+    with open("InformeProbTecnico.txt", "r") as f: 
+    #leer todos los datos y mostrarlos
+        st.write(f.readlines())
+
+if "counter" not in st.session_state:
+    st.session_state.counter=0
+
+if st.button("Contar"):
+    st.session_state.counter +=1
+st.write(f"{st.session_state.counter} veces que pinchamos")    
+
+st.write(eval("10+10==21"))
+st.write("Instrucciones: usar st.write() para imprimir :v:")
+
+
+codigo= st.text_area('Introducir codigo')
+
+if codigo:
+    exec(codigo)
