9252ecb71d1346b169d7fcd25ea409f9480abd6d
Daisy <dras2002@hotmail.com>
2024-03-05 17:52:20 +0100

    03_05_2024

--- Clase204_03_24.py
+++ Clase204_03_24.py
@@ -1,46 +1,42 @@
 #Ejercicio pag 87
 #RETO:  Sistema para calcular las nóminas
 
-
-class Sistema_Nominas:
-    #parametro- una lista de clase Empleado
-    def calcular_nominas(self, empleados):
-        print('Calculando nominas')
-        print('###################')
-        for empleado in empleados:
-            print(f'Nomina para: {empleado.name} - {empleado.lenguaje}')
-            print(f'- $ : {empleado.calcular_nomina()}')
-            print('')
-
-
-class empleados():
-    def __init__(self, nombre, salario=float, programador=str, analista=str, scrum_master=str):
-        self.nombre= nombre
+class empleado():
+    def __init__(self, name, salario, languaje):
+        self.name= name
         self.salario= salario
+        self.languaje= languaje
+      
+
+    def Calcular_Salario(self):
+        return self.salario*1.1
+
+class posicion():
+    def __init__(self, programador, analista, scrum_master):
         self.programador= programador
         self.analista= analista
         self.scrum_master= scrum_master
-
-    def Calcular_Salario(self):
-        print(f"Tu salario es: ", {self.salario*1.1})
-
-
-
-
-
-
-empleados=[]
-
-"""empleados.append(jon)
-empleados.append(maria)
-empleados.append(pepe)"""
-
-
-
-
-
+       
+class Sistema_Nominas(empleado, posicion):
+    #parametro- una lista de clase Empleado
+    def calcular_nominas(self, empleados):
+        print('Calculando nominas')
+        print('')
+        for empleado in empleados:
+            print(f'Nomina para: {self.name} - {self.languaje}')
+            print(f'- $ : {self.Calcular_Salario}')
+            print('')
 
 #Ejecutar el metodo para calcular los salarios
+
+deisy=("deisy", 50000, "python")
+maria= ("maria", 25000,"java")
+karlos= ("karlos",20000,  "c++")
+
+empleados=[]
+empleados.append(deisy)
+empleados.append(maria)
+empleados.append(karlos)
+
 nominas= Sistema_Nominas()
-nominas.calcular_nominas(empleados)
-        
+nominas.calcular_nominas(empleados)       
