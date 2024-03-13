25708296886c9fca1bfd93777ff01bed141b6ab4
Daisy <dras2002@hotmail.com>
2024-03-13 17:51:11 +0100

    13_03_2024

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 
-from flask import Flask
+from flask import Flask, render_template, request, url_for, redirect
+
 app= Flask(__name__)
 
 @app.route("/")
@@ -10,6 +11,26 @@ def homepage():
 def agur():
     return "<h1>AGUR</h1> <a href= '/'> Click aqui </a> para ir a Home Page  "
 
+@app.route("/dashboard/<string:username>")
+def dashboard(username):
+    return f"Ya estas dentro {username}!"
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if request.method=="GET":
+        return render_template("login.html")    
+    else: #POST
+        username = request.form["username"]
+        password = request.form["password"]
+
+        if username == username and password=="password":
+            return redirect(url_for("dashboard", username= username))
+        else:
+            return "No puedes entrar. Contraseña o usuario incorrecto"
+
+
+        return f"submit resultados con POST {username} y {password}"
+    
 
 if "__name__" == '_main_':
     app.run()
--- m1.py
+++ m1.py
@@ -2,7 +2,7 @@ productos=["raqueta", "balon", "guantes", "pelota"]
 
 #crear una funcion para convertir la primera en mayuscula
 
-prodMay=[]
+"""prodMay=[]
 
 def convertir_may(s):
     return s.capitalize()
@@ -10,4 +10,51 @@ def convertir_may(s):
 #2. aplicarla a todos los elementos de la lista
 for p in productos:
     print(convertir_may(p))
+
+newp=[convertir_may(p) for p in productos]
+print(newp) """
    
+
+   #otra forma
+"""
+captext= lambda x:x.capitalize()
+print(captext("raqueta"))"""
+
+
+#ahora con map
+
+
+"""def convertir_may(s):
+    return s.capitalize()
+
+print(list(map(convertir_may, productos))) """
+
+"""def convertir_may(s):
+    return s.capitalize()
+print(list(map(lambda x:x.capitalize(), productos)))   """
+
+#ejercicio 1: sumar 2 a cada elemento de la lista [4,5,6]= [6,7,8]
+listnum=[4,5,6]
+
+print(list(map(lambda x: x+2, listnum)))
+
+# ó,  es lo mismo que arriba
+def s2(x):
+    return  x+2
+print(list(map(s2,listnum)))
+    
+
+
+#ejercicio 2: import math para aprovechar de la funcion math.sqrt
+import math 
+x= tuple(map(math.sqrt, listnum))
+print(x)
+
+#ejercicio 3: teniendo 3 listas de numeros, multiplicar cada numero
+x=[4,5,6]
+y=[6,3,1]
+z=[8,1,2]
+
+lambda a, b, c: a*b*c
+
+print(tuple(map(lambda a, b, c: a*b*c, x,y,z)))
